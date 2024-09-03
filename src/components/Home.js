7b1652c1758cddb4e3a40cae3c81af72be334f12
Ashutosh Jha <ashutoshjha2312@gmail.com>
2024-09-03 17:48:51 +0530

    logout button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook from react-router-dom
 import Navbar from './Navbar';
 import useVantaWaves from '../hooks/useVantaWaves';
 import './Home.css';
 
 const Home = () => {
   const vantaRef = useVantaWaves();
+  const navigate = useNavigate(); 
+
+  const handleLogout = () => {
+    navigate('/login'); 
+  };
 
   return (
     <div ref={vantaRef} className="vanta-background">
       <Navbar />
+      <button className="logout-button" onClick={handleLogout}>
+        Logout
+      </button>
       <h2>Welcome to the Home Page</h2>
       <p>You are logged in.</p>
     </div>
